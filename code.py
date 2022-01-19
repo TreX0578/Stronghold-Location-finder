4d9694bf67f2876607e58ec8ea44928b7828a9e6
TreX0578 <97933220+TreX0578@users.noreply.github.com>
2022-01-19 15:25:41 +0530

    Update code.py
    Code correction, removed + - sign error.

--- code.py
+++ code.py
@@ -1,21 +1,49 @@
 p=float(input("Enter the pixel change: "))
-
-d=int(2250/p)
-
-print("Rough distance= ",d,"blocks")
-
+d=(2250/p)
+print("Rough distance= ",int(d),"blocks \n",int(d/8)," blocks in the nether")
 x1=int(input("Enter your X of first standing point: "))
-
 z1=int(input("Enter your Z of first standing point: "))
-
 x2=int(input("Enter your X of second standing point: "))
-
 z2=int(input("Enter your Z of second standing point: "))
-
 m=(z2-z1)/(x2-x1)
-
-x3=int(d/((1+m*m)**(1/2))+x1)
-
-z3=int((d*m)/((1+m*m)**(1/2))+z1)
-
-print("Rough stronghold position(X,Z) = (",x3,",",z3,")\n Nether position of stronghold = (",int(x3/8),",",int(z3/8),")")
+r=(1+m*m)**(1/2)
+pos_x3=(d/r)+x1
+pos_z3=((d*m)/r)+z1
+neg_x3=-1*(d/r)+x1
+neg_z3=-1*((d*m)/r)+z1
+diff_x=x1-x2
+diff_z=z1-z2
+if diff_x>diff_z:
+  if x1>x2:
+    print("Rough stronghold position(X,Z) = (",int(neg_x3),",",int(neg_z3),")\n Nether position of stronghold = (",int(neg_x3/8),",",int(neg_z3/8),")")
+  if x2>x1:
+    print("Rough stronghold position(X,Z) = (",int(pos_x3),",",int(pos_z3),")\n Nether position of stronghold = (",int(pos_x3/8),",",int(pos_z3/8),")")
+  else:
+    print("ERROR! CORDINATES SEEMS TO BE SAME, RUN THE CODE AGAIN WITH CORRECT VALUES")
+if diff_z>diff_x:
+  if z1>z2:
+    print("Rough stronghold position(X,Z) = (",int(neg_x3),",",int(neg_z3),")\n Nether position of stronghold = (",int(neg_x3/8),",",int(neg_z3/8),")")
+  if z2>z1:
+    print("Rough stronghold position(X,Z) = (",int(pos_x3),",",int(pos_z3),")\n Nether position of stronghold = (",int(pos_x3/8),",",int(pos_z3/8),")")
+  else:
+    print("ERROR! CORDINATES SEEMS TO BE SAME, RUN THE CODE AGAIN WITH CORRECT VALUES")
+else:
+  if z1>z2:
+    print("Rough stronghold position(X,Z) = (",int(neg_x3),",",int(neg_z3),")\n Nether position of stronghold = (",int(neg_x3/8),",",int(neg_z3/8),")")
+  if z2>z1:
+    print("Rough stronghold position(X,Z) = (",int(pos_x3),",",int(pos_z3),")\n Nether position of stronghold = (",int(pos_x3/8),",",int(pos_z3/8),")")
+  else:
+    print("ERROR! CORDINATES SEEMS TO BE SAME, RUN THE CODE AGAIN WITH CORRECT VALUES")
+  
+  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
