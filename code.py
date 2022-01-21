b25cc4d542beedae26eba847ad8a49bd4c7a147b
TreX0578 <97933220+TreX0578@users.noreply.github.com>
2022-01-21 19:34:42 +0530

    Update code.py
    Not sure if this code is fully right, and also more work needed on constant of in d

--- code.py
+++ code.py
@@ -11,24 +11,7 @@ pos_x3=(d/r)+x1
 pos_z3=((d*m)/r)+z1
 neg_x3=-1*(d/r)+x1
 neg_z3=-1*((d*m)/r)+z1
-diff_x=x1-x2
-diff_z=z1-z2
-if diff_x>diff_z:
-  if x1>x2:
-    print("Rough stronghold position(X,Z): (",int(neg_x3),",",int(neg_z3),")\n Nether position of stronghold: (",round(neg_x3/8),",",round(neg_z3/8),")")
-  if x2>x1:
-    print("Rough stronghold position(X,Z): (",int(pos_x3),",",int(pos_z3),")\n Nether position of stronghold: (",round(pos_x3/8),",",round(pos_z3/8),")")
-  if x1==x2:
-    print("ERROR! CORDINATES SEEMS TO BE SAME, RUN THE CODE AGAIN WITH CORRECT VALUES")
-if diff_z>diff_x:
-  if z1>z2:
-    print("Rough stronghold position(X,Z): (",int(neg_x3),",",int(neg_z3),")\n Nether position of stronghold: (",round(neg_x3/8),",",round(neg_z3/8),")")
-  if z2>z1:
-    print("Rough stronghold position(X,Z): (",int(pos_x3),",",int(pos_z3),")\n Nether position of stronghold: (",round(pos_x3/8),",",round(pos_z3/8),")")
-  if z1==z2:
-    print("ERROR! CORDINATES SEEMS TO BE SAME, RUN THE CODE AGAIN WITH CORRECT VALUES")
-if diff_x==diff_z:
-  if z1>z2:
-    print("Rough stronghold position(X,Z): (",int(neg_x3),",",int(neg_z3),")\n Nether position of stronghold: (",round(neg_x3/8),",",round(neg_z3/8),")")
-  if z2>z1:
-    print("Rough stronghold position(X,Z): (",int(pos_x3),",",int(pos_z3),")\n Nether position of stronghold: (",round(pos_x3/8),",",round(pos_z3/8),")")
+if (x2>x1 and z2<z1) or (x2<x1 and z2<z1):
+  print("Rough stronghold position(X,Z): (",int(pos_x3),",",int(pos_z3),")\n Nether position of stronghold: (",round(pos_x3/8),",",round(pos_z3/8),")")
+if (x2<x1 and z2>z1) or (x2>x1 and z2>z1):
+  print("Rough stronghold position(X,Z): (",int(neg_x3),",",int(neg_z3),")\n Nether position of stronghold: (",round(neg_x3/8),",",round(neg_z3/8),")")
